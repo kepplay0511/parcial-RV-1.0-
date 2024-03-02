6e5a5bd70a701a041fcdb4062438eda95a955aa3
Alejandro Sarria Diaz <142643925+kepplay0511@users.noreply.github.com>
2024-03-02 00:35:50 -0500

    funcionalidad al 50%
    revisar gravedad
    y manejo de las barritas

--- Assets/scripts/player controller.cs
+++ Assets/scripts/player controller.cs
@@ -15,16 +15,35 @@ public class player : MonoBehaviour
 
 
     public float side_speed;
+    public float running_speed;
+    public float jump_Force;
+    public float baseSpeed = 10f; // Velocidad base del personaje
+    public float speedIncrease = 0.1f; // Aumento de velocidad al tocar un objeto (10%)
+
+    private float currentSpeed; // Velocidad actual del personaje
+
+
+    [SerializeField] Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         currunt_pos = 0;  // 0 = center, 1 =  left , 2 =  right,
+        currunt_pos = 0;
+        rb = GetComponent<Rigidbody>(); // Obtener el componente Rigidbody del GameObject
+        rb.useGravity = true; // Activar la gravedad en el Rigidbody
+        currentSpeed = baseSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb.AddForce(Vector3.down * Physics.gravity.y * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + running_speed * Time.deltaTime);
+
+
+
         if (currunt_pos == 0)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -56,19 +75,43 @@ public class player : MonoBehaviour
 
         }
         if (currunt_pos == 0)
+
+          
         {
             Vector3 dir =  new Vector3(center.position.x, transform.position.y, transform.position.z) - transform.position ;
             transform.Translate(dir.normalized * side_speed * Time.deltaTime,Space.World);
         }
+
         else if (currunt_pos == 1)
         {
+                
+                { 
             Vector3 dir =  new Vector3(left.position.x, transform.position.y, transform.position.z) - transform.position;
             transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
-        }
+                }
+               
+            }
         else if (currunt_pos == 2)
         {
-            Vector3 dir =  new Vector3(right.position.x, transform.position.y, transform.position.z) - transform.position;
+                
+                { 
+                    Vector3 dir =  new Vector3(right.position.x, transform.position.y, transform.position.z) - transform.position;
             transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+                  }  
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jump_Force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) // Reemplaza "E" por la tecla de interacción
+        {
+            currentSpeed *= (1f + speedIncrease); // Aumenta la velocidad en un 10%
         }
     }
+    public float GetCurrentSpeed()
+    {
+        return currentSpeed;
+    }
+
 }
