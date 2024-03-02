2741de625c9c2ca63f0d9578b98ee9a19b373354
Alejandro Sarria Diaz <142643925+kepplay0511@users.noreply.github.com>
2024-03-02 03:58:48 -0500

    panel game over funcional mas contador en cuenta regresiva
    1.4

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -5,14 +5,39 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timer = 0;
+    public float tiempoInicial = 60f; // Tiempo inicial en segundos
+    private float timer;
+    public Text textoTimer;
+    public GameObject panelGameOver; // Panel que se mostrará al finalizar el tiempo
+    private bool tiempoTerminado = false;
 
-    public Text textoTimer; 
-    // Update is called once per frame
+    void Start()
+    {
+       
+            timer = tiempoInicial;
+            panelGameOver.SetActive(false); // Desactivar el panel de Game Over al iniciar el juego
+        
+
+    }
+
     void Update()
     {
-        timer -= Time.deltaTime;
+        if (!tiempoTerminado)
+        {
+            timer -= Time.deltaTime;
+            textoTimer.text = Mathf.RoundToInt(timer).ToString();
 
-        textoTimer.text = "" + timer.ToString("f0");
+            if (timer <= 0)
+            {
+                tiempoTerminado = true;
+                MostrarPanelGameOver();
+                Time.timeScale = 0f; // Pausar el juego al finalizar el tiempo
+            }
+        }
+    }
+
+    void MostrarPanelGameOver()
+    {
+        panelGameOver.SetActive(true); // Mostrar el panel de Game Over
     }
 }
